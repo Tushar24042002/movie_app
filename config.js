7d4d6af150b9352860b70f31414ab69e7ce1eec6
Tushar <100976552+Tushar24042002@users.noreply.github.com>
2024-07-04 00:00:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,9 @@
-// import { Sequelize } from "sequelize";
 
-//  const sequelize = new Sequelize('sql12716419', 'sql12716419', 'wFdP81CnQz', {
-//   host: 'sql12.freemysqlhosting.net	',
-//   dialect: 'mysql',
-//   port: 3306,
-// });
-
-// export default sequelize;
 
 
 // database.js
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('mysql://root:lLPtJNHrdxvEOVlyXqMVvYDKajLUdGiy@roundhouse.proxy.rlwy.net:54665/railway');
+const sequelize = new Sequelize('url');
 
 export default sequelize;
